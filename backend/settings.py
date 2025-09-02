3c002fdbb4693b70401dafd76a246220460bbe9e
maZen04 <124106109+maZen04@users.noreply.github.com>
2025-09-02 06:11:57 +0300

    Update Allowed Hosts

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'high-janaye-mazengalal30-162bb961.koyeb.app',
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
